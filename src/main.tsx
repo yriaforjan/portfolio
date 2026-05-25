9508204ff39323e8c2a7b5181ee7c9b788d6c292
Yria Forján Oliveira <yriaforjan.oliveira@gmail.com>
2026-05-25 21:36:52 +0200

    fix: force scroll to top on reload and disable browser scroll restoration

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 
+history.scrollRestoration = "manual";
+window.scrollTo(0, 0);
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
